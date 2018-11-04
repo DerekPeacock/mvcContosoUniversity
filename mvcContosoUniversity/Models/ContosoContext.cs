4d631ef460e79cf107ec6bb4a7db78c04d985195
DerekPeacock <derek.peacock@yahoo.com>
2018-11-04 14:40:41 +0000

    Context class and Initialiser class added with sample data

---
+++ mvcContosoUniversity/Models/ContosoContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace mvcContosoUniversity.Models
+{
+    public class ContosoContext : DbContext
+    {
+        public ContosoContext() : base("DefaultContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrolment> Enrolments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        //}
+    }
+}
